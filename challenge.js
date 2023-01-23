31033ded0e025f174334b7fea69dd4b50b12006b
Ghilain <ishimweghilain5050@gmail.com>
2023-01-23 09:07:17 +0200

    add condition to check target

--- challenge.js
+++ challenge.js
@@ -9,7 +9,15 @@ var searchInsert = function(nums, target) {
         }
         else{
             for( let i = 0; i < nums.length; i++){
-
+                if( target < nums[0]){
+                    return 0;
+                    }
+                    else if( target > nums[nums.length-1]){
+                    return nums.length;
+                    }
+                    else if(target > nums[i] && target < nums[i+1] ){
+                    return i+1;
+                    }
             }
             }
     };
