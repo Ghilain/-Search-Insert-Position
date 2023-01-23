7357d62ab45ae2958e68fabf9a3e8d84579cd945
Ghilain <ishimweghilain5050@gmail.com>
2023-01-23 09:06:00 +0200

    add loop to check in array

--- challenge.js
+++ challenge.js
@@ -7,5 +7,9 @@ var searchInsert = function(nums, target) {
     if(nums.includes(target)){
         return nums.indexOf(target);
         }
+        else{
+            for( let i = 0; i < nums.length; i++){
 
+            }
+            }
     };
